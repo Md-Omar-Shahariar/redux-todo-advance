283d32d3c094d234a06cb8423a1a000e9d5b9b53
Md-Omar-Shahariar <osafridi00@gmail.com>
2023-01-02 15:16:25 +0600

    fetch todoss middleware

--- middleware.js
+++ middleware.js
@@ -1,3 +1,4 @@
+const fetch = require("node-fetch");
 const delayAction = (store) => (next) => (action) => {
   if (action.type === "todos/todoAdded") {
     console.log("I am delaying You");
@@ -11,8 +12,17 @@ const delayAction = (store) => (next) => (action) => {
   return next(action);
 };
 
-const fetchTodosMiddleware = (store) => (next) => (action) => {
+const fetchTodosMiddleware = (store) => (next) => async (action) => {
   if (action.type === "todos/fetchTodos") {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos?_limit=5"
+    );
+    const todos = await response.json();
+    store.dispatch({
+      type: "todos/todoLoaded",
+      payload: todos,
+    });
+    return;
   }
   return next(action);
 };
